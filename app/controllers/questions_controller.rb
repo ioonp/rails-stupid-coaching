0dc67941b008aa119458e6ae096dee1eb5878f0f
ion iapara <iapara.ion.g@gmail.com>
2022-05-16 16:52:57 +0200

    second challenge

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,22 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @answer = params[:answer]
+
+    if @answer == 'I am going to work'
+      @message = 'Great!'
+
+    elsif @answer.include?('?')
+      @message = 'Silly question, get dressed and go to work!.'
+
+    else
+      @message = "I don't care, get dressed and go to work!"
+    end
+
+    
+  end
+
+end
